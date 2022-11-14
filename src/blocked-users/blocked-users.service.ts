319080e71433f63a11cf770f9e2300562a6fcd01
benloco <benquarshie2@gmail.com>
2022-11-14 21:42:07 +0000

    fix

--- src/blocked-users/blocked-users.service.ts
+++ src/blocked-users/blocked-users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
-import { BlockUserInput } from './dto/bocked-users.input'
+import { BlockUserInput } from './dto/blocked-users.input'
 import { BlockedUsers } from './entities/blocked-users.entity'
 
 @Injectable()
