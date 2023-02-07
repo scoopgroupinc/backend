0167ecdc6fee716e5687a3c261db5cec07c9cd90
Benloco <74012997+Benloco@users.noreply.github.com>
2023-02-07 14:52:45 +0000

    user swipe action

--- src/user-choice/user-choice.resolver.ts
+++ src/user-choice/user-choice.resolver.ts
@@ -1,5 +1,5 @@
 import { UseGuards } from '@nestjs/common'
-import { Args, Resolver, Query } from '@nestjs/graphql'
+import { Args, Resolver, Query, Mutation } from '@nestjs/graphql'
 import { GqlAuthGuard } from 'src/auth/guards/jwt-auth.guard'
 import { UserChoiceOutput } from './dto/user-choices.output'
 import { UserChoiceService } from './user-choice.service'
@@ -14,5 +14,14 @@ export class UserChoiceResolver {
         @Args('userId') userId: string
     ): Promise<UserChoiceOutput[]> {
         return await this.userChoiceService.getUserChoices(userId)
+    }
+
+    @UseGuards(GqlAuthGuard)
+    @Mutation(() => String)
+    async userSwipeAction(
+        @Args('matchId') matchId: string,
+        @Args('choice') choice: string
+    ){
+        return await this.userChoiceService.userSwipeAction(matchId, choice)
     }
 }
