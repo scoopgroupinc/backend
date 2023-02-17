b406b5f11480c3be2ee88366cf2033763ac49ce1
Benloco <74012997+Benloco@users.noreply.github.com>
2023-02-17 13:25:05 +0000

    Scoop 329 (#72)
    * scoop-329 match ability to chat
    
    * update

--- src/user-choice/user-choice.service.ts
+++ src/user-choice/user-choice.service.ts
@@ -45,16 +45,17 @@ export class UserChoiceService {
         const allChoices = []
         for (const choice of choices) {
             const user = await this.userService.findOneByID(choice.shownUserId)
-
-            const profile = await this.userProfileService.findOne(
-                choice.shownUserId
-            )
-            allChoices.push({
-                ...choice,
-                choiceName: `${user.firstName} ${user.lastName}`,
-                gender: profile.gender,
-                age: moment().diff(profile.birthday, 'years', false),
-            })
+            if (user) {
+                const profile = await this.userProfileService.findOne(
+                    choice.shownUserId
+                )
+                allChoices.push({
+                    ...choice,
+                    choiceName: `${user.firstName} ${user.lastName}`,
+                    gender: profile.gender,
+                    age: moment().diff(profile.birthday, 'years', false),
+                })
+            }
         }
         let uniqueMatches = []
         allChoices.forEach((item) => {
