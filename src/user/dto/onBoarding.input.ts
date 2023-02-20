aa54bce8e5b338ab7a7eb74462f744f039e3a484
Benloco <74012997+Benloco@users.noreply.github.com>
2023-02-20 11:19:03 +0000

    update onboarding (#74)

---
+++ src/user/dto/onBoarding.input.ts
@@ -0,0 +1,17 @@
+import { InputType, Field } from '@nestjs/graphql'
+import { IsOptional, IsString } from 'class-validator'
+
+@InputType()
+export class OnBoardInput {
+    @Field()
+    @IsString()
+    userId: string
+
+    @Field({ nullable: true })
+    @IsOptional()
+    onBoarding?: boolean
+
+    @Field({ nullable: true })
+    @IsOptional()
+    voteOnboard?: boolean
+}
