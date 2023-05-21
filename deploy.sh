45b96def406ee1e0e2a7ccbd0860aa02b6f7bb27
Benloco <74012997+Benloco@users.noreply.github.com>
2023-05-21 08:19:40 +0000

    Revert "change from npm to yarn"
    This reverts commit 01785b191198a853883c74224c2d22b1d077df27.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/backend 
+git add . && git commit -m "deploy"
 git pull origin development
-yarn install &&
-yarn build &&
+npm install &&
+npm run build &&
 pm2 restart 0
