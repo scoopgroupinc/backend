fb746787b51edea43cf4445b933ebef7285663bd
Natalie Pan <natalie.pan@gmail.com>
2023-08-03 18:19:05 -0700

    typo

--- src/user-tags/entities/user-tags.entity.ts
+++ src/user-tags/entities/user-tags.entity.ts
@@ -31,7 +31,7 @@ export class UserTagsEntity {
     @Field(() => UserTagsTypeVisibleEntity)
     @ManyToOne(
         () => UserTagsTypeVisibleEntity,
-        (userTagsTypeVisibleEnity) => userTagsTypeVisibleEnity.userTags
+        (userTagsTypeVisibleEntity) => userTagsTypeVisibleEntity.userTags
     )
     userTagsTypeVisible: UserTagsTypeVisibleEntity
 }
